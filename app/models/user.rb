fba76d1bc1d1097fbb293be1fb23932db7e1ddd3
Omwomo <eugeneoduor79@gmail.com>
2023-11-16 20:23:25 +0300

    Method for getting recent posts and updating comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence:
+
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
+
+  def update_posts_counter
+    update(posts_counter: posts.count)
+  end
 end
