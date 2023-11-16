ec70ce667865f65ba568d1aaa223a65435e4c66e
Omwomo <eugeneoduor79@gmail.com>
2023-11-16 20:25:09 +0300

    Methods to get recent comments, update commments and likes counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,16 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :text, presence: true
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
+
+  def update_comments_counter
+    update(comments_counter: comments.count)
+  end
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
 end
