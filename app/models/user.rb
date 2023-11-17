be7a4d1bb9c653007b4d68379b5a39e5cabb9e5c
Omwomo <eugeneoduor79@gmail.com>
2023-11-17 15:37:40 +0300

    Add validations for title atttribute, comments counter and likes couter methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,14 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
 
-  validates :name, presence:
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
+  end
+
+  def update_user_posts_counter
+    update(posts_counter: posts.count)
   end
 end
