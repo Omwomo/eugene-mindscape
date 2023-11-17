23b051d908465e6dfb19e3f4e2a6648cbfae9dd3
Omwomo <eugeneoduor79@gmail.com>
2023-11-17 19:29:51 +0300

    Write tests for attributes and methods in Comment model class

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,13 +4,14 @@ class Like < ApplicationRecord
 
   validates :user_id, presence: true
   validates :post_id, presence: true
-  validates :update_post_likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_post_likes_counter
 
   private
 
   def update_post_likes_counter
+    return unless post.present?
+
     post.increment!(:likes_counter)
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe Comment, type: :model do
     comment = build(:comment, text: nil)
     expect(comment).not_to be_valid
   end
+
+  describe 'update_post_comments_counter' do
+    let(:post) { create(:post) }
+
+    it 'increments comments_counter on creating a comment' do
+      expect {
+        create(:comment, post: post)
+        post.reload
+      }.to change { post.comments_counter }.by(1)
+    end
+  end
 end
