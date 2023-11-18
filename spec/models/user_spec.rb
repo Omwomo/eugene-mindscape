5e273e4636bfa066c139ec7f121479a11ca2e461
Omwomo <eugeneoduor79@gmail.com>
2023-11-18 09:53:57 +0300

    Write tests that returns recent posts with default limit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe User, type: :model do
     user = build(:user, posts_counter: -1)
     expect(user).not_to be_valid
   end
+
+  describe '#recent_posts' do
+    it 'returns recent posts with default limit' do
+      user = create(:user)
+      post1 = create(:post, author: user)
+      post2 = create(:post, author: user)
+      post3 = create(:post, author: user)
+
+      recent_posts = user.recent_posts
+
+      expect(recent_posts).to eq([post3, post2, post1])
+    end
+  end
 end
