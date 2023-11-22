05bbe080a9962cd0cb9b1f804e57c6d306ad04c2
Omwomo <eugeneoduor79@gmail.com>
2023-11-22 03:42:46 +0300

    Initiate contrller for Psts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+  def index
+    # logic to display all posts by a given user
+  end
+
+  def show
+    # logic to display a single post
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def index
+    # logic to display all users
   end
 
   def show
+    # logic to display a single user
   end
 end
