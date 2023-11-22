26798b2598013ad88f55279ab7cec9d8e602448d
Omwomo <eugeneoduor79@gmail.com>
2023-11-22 15:08:19 +0300

    Re-configured my routes to use resources instead

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  root 'home#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+ end
 end
