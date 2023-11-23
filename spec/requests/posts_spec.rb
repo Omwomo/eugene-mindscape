84368a5ef208224dbc5be446ffde39f49d7c6322
Omwomo <eugeneoduor79@gmail.com>
2023-11-23 03:31:27 +0300

    Write tests for posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
+RSpec.describe 'PostsController', type: :request do
+  let(:user) { create(:user) } # Create user from factory
+  let(:post) { create(:post, author: user) }
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'should render the correct response status' do
+      get user_posts_path(user_id: user.id)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render the correct Template' do
+      get user_posts_path(user_id: user.id)
+      expect(response).to render_template(:index)
+    end
+
+    it 'renders the correct placeholder text of the body' do
+      get user_posts_path(user_id: user.id)
+      expect(response.body).to include('All posts')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'should render the correct response status' do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render the correct Template' do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders the correct placeholder text of the body' do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response.body).to include('User post')
+    end
   end
 end
