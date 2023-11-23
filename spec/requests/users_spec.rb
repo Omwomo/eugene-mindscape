a9778d3d51e4be6c424146fba3c33cc0509c6059
Omwomo <eugeneoduor79@gmail.com>
2023-11-23 03:30:56 +0300

    Write tests for my user controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'UsersController', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    let(:user) { create(:user) }
+
+    it 'should render the correct response status' do
+      get users_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render the correct Template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'renders the correct placeholder text of the body' do
+      get users_path
+      expect(response.body).to include('ALL USERS')
+    end
+  end
+
+  describe 'GET /show' do
+    let(:user) { create(:user) } # Create user from factory
+
+    it 'should render the correct response status' do
+      get user_path(user)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render the correct Template' do
+      get user_path(user)
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders the correct placeholder text of the body' do
+      get user_path(user)
+      expect(response.body).to include('User Profile')
+    end
   end
 end
