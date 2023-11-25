f8a136d38c4cc397a0925db9a011778370b4419e
Omwomo <eugeneoduor79@gmail.com>
2023-11-25 09:31:16 +0300

    Creating a post on behalf of first user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,9 +11,23 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
   end
 
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path, notice: 'Post created!'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
