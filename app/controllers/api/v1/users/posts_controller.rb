7cb0f20199d66a0dcd3d37427a6f188de8143658
faisalakb <faisalakb786@gmail.com>
2023-12-02 12:42:32 +0500

    Add endpoints for users

---
+++ app/controllers/api/v1/users/posts_controller.rb
@@ -0,0 +1,13 @@
+module Api
+    module V1
+      class Users::PostsController < ApplicationController
+        # GET /api/v1/users/:user_id/posts
+        def index
+          @user = User.find(params[:user_id])
+          @posts = @user.posts
+  
+          render json: @posts
+        end
+      end
+    end
+  end
